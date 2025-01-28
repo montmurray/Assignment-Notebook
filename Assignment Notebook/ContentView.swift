dfcbf5d01516887d02d4b4a53de44af2ac94d12f
Tessa Murray <tmurray@bsd220.org>
2025-01-28 07:52:09 -0800

    Steps 1-5, added list array and necessary views.

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -7,13 +7,25 @@
 
 import SwiftUI
 
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
+}
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()), AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()), AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentItems) { item in Text(item.description)
+                }
+                .onMove(perform: { indices, newOffset in assignmentItems.move(fromOffsets: indices, toOffset: newOffset)})
+                
+                .onDelete(perform: { indexSet in assignmentItems.remove(atOffsets: indexSet)})
+            }
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
         .padding()
     }
