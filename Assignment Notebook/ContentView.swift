228a70db50dee4c6e2ed5cae6da9340a99eb7836
Tessa Murray <tmurray@bsd220.org>
2025-01-28 07:31:25 -0800

    Initial Commit

---
+++ Assignment Notebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment Notebook
+//
+//  Created by Tessa Murray on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
