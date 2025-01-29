1ef0e072396c5c0227bde37f0b0ce95ffc032d59
Tessa Murray <tmurray@bsd220.org>
2025-01-29 08:02:48 -0800

    Step 9, added addassignment view.

--- Assignment Notebook/AddAssignmentView.swift
+++ Assignment Notebook/AddAssignmentView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 
 struct AddAssignmentView: View {
     @ObservedObject var assignmentList: AssignmentList
+    @State private var showingAddAssignmentView = false
     @State private var course = ""
     @State private var description = ""
     @State private var dueDate = Date()
-    @Environment(\.presentationMode) var presentationMode
-    static let courses = ["Calculus", "History", "Chemistry", "Psychology", "English", "Computer Science"]
+    @Environment(\.presentationMode) var presentationMode; static let courses = ["Calculus", "History", "Chemistry", "Psychology", "Computer Science", "English", "Economics"]
     var body: some View {
         NavigationView {
             Form {
                 Picker("Course", selection: $course) {
-                    ForEach(Self.courses, id: \.self) { courses in Text(course) }
+                    ForEach(Self.courses, id: \.self) { course in Text(course) }
                 }
             }
+            .navigationBarTitle("Add New Assignment", displayMode: .inline)
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: { showingAddAssignmentView = true }, label: { Image(systemName: "plus")
+            }))
+            TextField("Description", text: $description)
+            DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
         }
-    }
+        }
     }
 
 #Preview {
--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -7,5 +7,5 @@
 import SwiftUI
 
 class AssignmentList: ObservableObject {
-    @Published var items = [AssignmentItem(course: "Algebra", description: "Linear Equation", dueDate: Date()), AssignmentItem(course: "History", description: "Civil War Paper", dueDate: Date()), AssignmentItem(course: "Science", description: "Atomic Bomb Lab", dueDate: Date())]
+    @Published var items = [AssignmentItem(course: "Calculus", description: "Related Rates Quiz", dueDate: Date()), AssignmentItem(course: "History", description: "Ottoman Empire Paper", dueDate: Date()), AssignmentItem(course: "Chemistry", description: "Periodic Table", dueDate: Date()), AssignmentItem(course: "Pyschology", description: "Two-Factor Theory Discussion", dueDate: Date()), AssignmentItem(course: "Computer Science", description: "Assignment Notebook", dueDate: Date())]
     }
