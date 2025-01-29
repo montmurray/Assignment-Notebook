2d729986a233c6827ba7894ac72baa11e01e2820
Tessa Murray <tmurray@bsd220.org>
2025-01-29 07:37:05 -0800

    Added assignment view, step 8.

---
+++ Assignment Notebook/AddAssignmentView.swift
@@ -0,0 +1,30 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook
+//
+//  Created by Tessa Murray on 1/29/25.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["Calculus", "History", "Chemistry", "Psychology", "English", "Computer Science"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Course", selection: $course) {
+                    ForEach(Self.courses, id: \.self) { courses in Text(course) }
+                }
+            }
+        }
+    }
+    }
+
+#Preview {
+    AddAssignmentView(assignmentList: AssignmentList())
+}
