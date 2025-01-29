baa0a99623654d90ffedd9f9131c655ad2fe490b
Tessa Murray <tmurray@bsd220.org>
2025-01-29 08:43:44 -0800

    I give up. Started the whole thing over.

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,38 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var assignmentList = AssignmentList()
-    @State private var showingAddAssignmentView = false
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(assignmentList.items) { item in HStack {
-                    VStack(alignment: .leading, content: {
-                        Text(item.course).font(.headline)
-                        Text(item.description)
-                    })
-                    Spacer()
-                    Text(item.dueDate, style: .date)
-                }
-                }
-                .onMove(perform: { indices, newOffset in assignmentList.items.move(fromOffsets: indices, toOffset: newOffset)})
-                
-                .onDelete(perform: { indexSet in assignmentList.items.remove(atOffsets: indexSet)})
-            }
-            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
-            .sheet(isPresented: $showingAddAssignmentView, content: { AddAssignmentView(assignmentList: assignmentList)
-            })
-            .navigationBarItems(leading: EditButton(), trailing: Button(action: { showingAddAssignmentView = true }, label: { Image(systemName: "plus")
-            }))
-        }
-    }
+    @State private var assignments = ["Paper", "Discussion", "Quiz"]
 }
-struct AssignmentItem: Identifiable, Codable {
-    var id = UUID()
-    var course = String()
-    var description = String()
-    var dueDate = Date()
-}
+
 #Preview {
     ContentView()
 }
