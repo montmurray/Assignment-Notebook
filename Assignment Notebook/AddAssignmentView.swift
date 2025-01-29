a068b094402095100178e14ef84e3b23b5eae699
Tessa Murray <tmurray@bsd220.org>
2025-01-29 08:34:45 -0800

    HK 1...

--- Assignment Notebook/AddAssignmentView.swift
+++ Assignment Notebook/AddAssignmentView.swift
@@ -19,20 +19,20 @@ struct AddAssignmentView: View {
                 Picker("Course", selection: $course) {
                     ForEach(Self.courses, id: \.self) { course in Text(course) }
                 }
+                .navigationBarTitle("Add New Assignment", displayMode: .inline)
+                .navigationBarItems(trailing: Button("Save") {
+                    if course.count > 0 && description.count > 0 {
+                        let item = AssignmentItem(id: UUID(), course: course, dueDate: dueDate)
+                        assignmentList.items.append(item)
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                })
+                TextField("Description", text: $description)
+                DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
             }
-            .navigationBarTitle("Add New Assignment", displayMode: .inline)
-            .navigationBarItems(trailing: Button("Save") {
-                if course.count > 0 && description.count > 0 {
-                    let item = AssignmentItem(id: UUID(), course: course, dueDate: dueDate)
-                    assignmentList.items.append(item)
-                    presentationMode.wrappedValue.dismiss()
-                }
-            })
-            }
-            TextField("Description", text: $description)
-            DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
         }
     }
+}
 
 #Preview {
     AddAssignmentView(assignmentList: AssignmentList())
